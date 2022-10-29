7613a98f61d503e45652e288ae8321c41fcc79d8
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-10-29 10:40:18 -0700

    Complete exercise 2 extra credit 1

--- src/__tests__/exercise/02.js
+++ src/__tests__/exercise/02.js
@@ -3,6 +3,7 @@
 
 import * as React from 'react'
 import {render, fireEvent} from "@testing-library/react"
+import {toHaveTextContent} from "@testing-library/jest-dom"
 import Counter from '../../components/counter'
 
 test('counter increments and decrements when the buttons are clicked', () => {
@@ -12,11 +13,11 @@ test('counter increments and decrements when the buttons are clicked', () => {
   const [decrement, increment] = container.querySelectorAll('button')
   const message = container.firstChild.querySelector('div')
 
-  expect(message.textContent).toBe('Current count: 0')
+  expect(message).toHaveTextContent('Current count: 0')
 
 	fireEvent.click(increment);
-  expect(message.textContent).toBe('Current count: 1')
+  expect(message).toHaveTextContent('Current count: 1')
 
 	fireEvent.click(decrement);
-  expect(message.textContent).toBe('Current count: 0')
+  expect(message).toHaveTextContent('Current count: 0')
 })
