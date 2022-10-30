77879ab35d6d81fe4bc078c137167aa67b582b49
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-10-30 16:42:56 -0700

    Complete exercise 8 extra credit 2

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -6,43 +6,43 @@ import {render, screen, act} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import useCounter from '../../components/use-counter'
 
-function UseCounterHookExample() {
-  const {count, increment, decrement} = useCounter()
-  return (
-    <div>
-      <div>Current count: {count}</div>
-      <button onClick={decrement}>Decrement</button>
-      <button onClick={increment}>Increment</button>
-    </div>
-  )
-}
-
-test('exposes the count and increment/decrement functions', async () => {
-  let result
+function setup({initialProps} = {}) {
+  const result = {}
   function TestComponent() {
-    result = useCounter()
+    result.current = useCounter(initialProps)
     return null
   }
   render(<TestComponent />)
-  expect(result.count).toBe(0)
-  act(() => result.increment())
-  expect(result.count).toBe(1)
-  act(() => result.decrement())
-  expect(result.count).toBe(0)
+  return result
+}
 
-  // const increment = screen.getByRole('button', {name: /increment/i})
-  // const decrement = screen.getByRole('button', {name: /decrement/i})
-  // const message = screen.getByText(/current count/i)
+test('exposes the count and increment/decrement functions', async () => {
+  const result = setup()
 
-  // expect(message).toHaveTextContent('Current count: 0')
-  // await userEvent.click(increment)
-  // expect(message).toHaveTextContent('Current count: 1')
-  // await userEvent.click(decrement)
-  // expect(message).toHaveTextContent('Current count: 0')
-  // 🐨 render the component
-  // 🐨 get the elements you need using screen
-  // 🐨 assert on the initial state of the hook
-  // 🐨 interact with the UI using userEvent and assert on the changes in the UI
+  expect(result.current.count).toBe(0)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(1)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(0)
+})
+
+test('allows customization of initial count', async () => {
+  const result = setup({initialProps: {initialCount: 3}})
+
+  expect(result.current.count).toBe(3)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(4)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(3)
+})
+
+test('allows customization of the step', async () => {
+  const result = setup({initialProps: {step: 2}})
+  expect(result.current.count).toBe(0)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(2)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(0)
 })
 
 /* eslint no-unused-vars:0 */
