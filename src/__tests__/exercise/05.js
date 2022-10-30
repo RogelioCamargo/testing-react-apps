083c01a7ac6d9e28d09d44aff9e3afc87f001d0e
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-10-29 19:50:16 -0700

    Complete exercise 5 extra credit 4

--- src/__tests__/exercise/05.js
+++ src/__tests__/exercise/05.js
@@ -7,7 +7,7 @@ import {waitForElementToBeRemoved} from '@testing-library/react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import {build, fake} from '@jackfranklin/test-data-bot'
-// 🐨 you'll need to import rest from 'msw' and setupServer from msw/node
+import {rest} from 'msw'
 import {handlers} from 'test/server-handlers'
 import {setupServer} from 'msw/node'
 import Login from '../../components/login-submission'
@@ -22,6 +22,7 @@ const buildLoginForm = build({
 const server = setupServer(...handlers)
 
 beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
 afterAll(() => server.close())
 
 test(`logging in displays the user's username`, async () => {
@@ -49,4 +50,23 @@ test('omitting the password results in an error', async () => {
   expect(screen.getByRole('alert').textContent).toMatchInlineSnapshot(
     `"password required"`,
   )
+})
+
+test('unknown server error displays the error message', async () => {
+	const testErrorMessage = "Oh no, something bad happened"
+  server.use(
+    rest.post(
+      'https://auth-provider.example.com/api/login',
+      async (req, res, ctx) => {
+        return res(ctx.status(500), ctx.json({message: testErrorMessage}))
+      },
+    ),
+  )
+
+  render(<Login />)
+  await userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+
+  expect(screen.getByRole('alert')).toHaveTextContent(testErrorMessage)
 })
